e42ee92d27dea10bd11b44ff2e29397ea500466b
vladysPS <vladyslavk@pangostudio.com>
2026-06-04 10:16:08 +0200

    improved some logic

--- assets/explora-test.js
+++ assets/explora-test.js
@@ -1,41 +1,34 @@
 // @ts-nocheck
 class ExploraFerozLand extends HTMLElement {
-    constructor(){
+    constructor() {
         super();
         this.cardsArray = [];
         this.buttonsArray = [];
     }
 
-    connectedCallback(){
-        console.log("element constructed", this)
+    _handleButtonClick(e) {
+        const idButton = e.currentTarget.dataset.idButton;
+        this.cardsArray.forEach(card => {
+            card.classList.toggle("hidden", card.dataset.idCard !== idButton);
+        });
+        this.buttonsArray.forEach(btn => {
+            btn.classList.toggle("selected", btn.dataset.idButton === idButton);
+        });
+    }
+
+    connectedCallback() {
         this.cardsArray = this.querySelectorAll(".p-refugio-card");
         this.buttonsArray = this.querySelectorAll(".p-image-selector");
-        console.log("cards", this.cardsArray);
-        console.log("buttons", this.buttonsArray);
+
         this.buttonsArray.forEach(button => {
-            button.addEventListener("click", (e) => {
-                const idButton = e.currentTarget.dataset.idButton;
-                this.cardsArray.forEach(card => {
-                    const idCard = card.dataset.idCard;
-                    if(idCard === idButton){
-                        card.classList.remove("hidden");
-                    } else {
-                        card.classList.add("hidden");
-                    }
-                })
-                this.buttonsArray.forEach(btn => {
-                    const idBtn = btn.dataset.idButton;
-                    if(idBtn === idButton){
-                        btn.classList.add("selected");
-                    } else {
-                        btn.classList.remove("selected");
-                    }
-                })
-            })
-        })
+            button.addEventListener("click", this._handleButtonClick.bind(this));
+        });
     }
-    disconnectedCallback(){
-        console.log("element destroyed")
+
+    disconnectedCallback() {
+        this.buttonsArray.forEach(button => {
+            button.removeEventListener("click", this._handleButtonClick.bind(this));
+        });
     }
 }
 
